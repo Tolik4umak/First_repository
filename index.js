eea4e92fc59e9927480a640c6438a7b66d2086c1
AnnaShazk <dream1nd.find@gmail.com>
2022-07-01 23:03:55 +0200

    add final animation

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-click.onmouseover = function () {
-  let start = Date.now();
+let click = document.getElementsByClassName('click');
+console.log(click);
 
-  let timer = setInterval(function () {
-    let timePassed = Date.now() - start;
 
-    click.style.opacity = timePassed / 10 + "%";
+for (let i=0;i<click.length;i++){
 
-    if (timePassed > 1000) clearInterval(timer);
 
-    if (click.style.opacity > 0) {
-      click.onmouseout = function () {
-        clearInterval(timer);
-        let start1 = Date.now();
 
-        let timer1 = setInterval(function () {
-          let timePassed1 = Date.now() - start1;
+  
+  click[i].onmouseover = function () {
+    let start = Date.now();
+    let timer = setInterval(function () {
+      let timePassed = Date.now() - start;
+  
+      click[i].style.opacity = timePassed / 5 + "%";
+  
+      if (timePassed > 500) clearInterval(timer);
+     
+      if (click[i].style.opacity > 0) {
+        click[i].onmouseout = function () {
+          clearInterval(timer);
+          let start1 = Date.now();
+  
+          let timer1 = setInterval(function () {
+            let timePassed1 = Date.now() - start1;
+  
+            click[i].style.opacity = timePassed / 5 - timePassed1 / 5 + "%";
+  
+            if (timePassed1 > 500) clearInterval(timer1);
+          });
+        };
+      }
+    });
+  };
 
-          click.style.opacity = timePassed / 10 - timePassed1 / 10 + "%";
 
-          if (timePassed1 > 1000) clearInterval(timer1);
-        });
-      };
-    }
-  });
-};
+
+
+
+}
+
+
 
 // click.onmouseout = function () {
 //   let start = Date.now();
